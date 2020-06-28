ccaad6445b34aab28612d31fc0ccce28169a49f1
taylor1111 <esteban.delahoz15@gmail.com>
2020-06-28 09:13:32 +0000

    add base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Our BaseModel that defines all common attributes/methods for other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel():
+  """The main class"""
+
+  def __init__(self):    
+    self.id = str(uuid.uuid4())
+    self.created_at = datetime.now()
+    self.updated_at = datetime.now()
+  
+  def __str__(self):
+    return ("[{}] ({}) {}]".format(self.__class__.__name__, self.id, self.__dict__))
+
+  def save(self):
+    """Updates the public instance attribute updated_at with the current date and time"""
+    self.updated_at = datetime.now().isoformat()
+
+
+  def to_dict(self):
+    """Returns a dictionary containing all the __dict__ keys / values of the instance"""
+    my_dict = self.__dict__
+    my_dict.update({"__class__": self.__class__.__name__})
+    self.created_at = datetime.now().isoformat()
+    self.updated_at = datetime.now().isoformat()  
+    return (my_dict)

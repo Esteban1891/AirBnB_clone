534144c946a5eed78be9a80fad18ec588620f8b9
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-29 05:25:49 -0500

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,21 @@ from datetime import datetime
 
 class BaseModel():
     """The main class"""
-    def __init__(self):
+
+    def __init__(self, *args, **kwargs):
         """Constructor of the instance"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs is True:
+            for key in kwargs.keys():
+                if key == "__class__":
+                    continue
+                else:
+                    if key == "created_at" or key == "updated_at":
+                        datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return an user friendly representation

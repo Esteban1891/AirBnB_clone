d5a6594c259572648e64bfbf1d19ce2504c16461
Esteban1891 <esteban.delahoz15@gmail.com>
2020-06-30 08:27:29 -0500

    task6

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""that contains the entry point of the command interpreter
+"""
+import cmd
+import sys
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, args):
+        """Quit command to exit the program\n"""
+        raise SystemExit
+
+    def do_EOF(self, args):
+        """Handles end of file"""
+        return True
+
+    def do_help(self, args):
+        """help"""
+        cmd.Cmd.do_help(self, args)
+
+    def emptyline(self):
+        """empty lines"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()

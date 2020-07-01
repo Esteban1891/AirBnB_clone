023ac94ccbdad5e3f7fff8220f88038f3cb7149a
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-01 08:13:34 +0000

    Fix base_model and fix task 5

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel():
@@ -22,6 +23,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return an user friendly representation
@@ -34,6 +36,7 @@ class BaseModel():
         """Updates the public instance attribute
         updated_at with the current date and time"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing

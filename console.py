a20414a270069e4b5315d2c7d48ab3dd747d99fa
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-02 04:48:53 +0000

    Addes create command to console (task 7)

--- console.py
+++ console.py
@@ -19,6 +19,19 @@ class HBNBCommand(cmd.Cmd):
     def do_help(self, args):
         """help"""
         cmd.Cmd.do_help(self, args)
+    
+    def do_create(self, args):
+        """Creates a new instance of Basemodel,
+        saves it (to the JSON file) and prints the
+        id"""        
+        if not args:
+            print("** class name missing **")
+        elif args != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            new_instance = BaseModel()
+            models.storage.save()
+            print(new_instance.id)
 
     def emptyline(self):
         '''dont execute anything when user

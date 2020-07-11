a4e187fc7c636a3e045210eaf71e7c7508867770
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-11 08:58:17 +0000

    Added some new classes that inherit from BaseModel

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""City class that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Our beautiful City class"""
+    state_id = ""
+    name = ""

b1f852a83193608c0c28a3fe1aba6de4d6e7e18e
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-11 08:39:14 +0000

    Fix pep8 filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ import json
 from models.base_model import BaseModel
 from models.user import User
 
+
 class FileStorage:
     """Our beautiful class"""
 
--- models/user.py
+++ models/user.py
@@ -9,7 +9,3 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """User constructor"""
-        super().__init__(*args, **kwargs)

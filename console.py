1e740dfdf892b8b0932670f79cd0f21e4c686f58
EduardoVega04 <eduarvega04@hotmail.com>
2020-07-11 09:18:26 +0000

    All files now are executables

--- console.py
+++ console.py
@@ -8,7 +8,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from model.review import Review
+from models.review import Review
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
